241ababf8ba67726ebcc8312241c589196657731
saruni-spec <54449536+saruni-spec@users.noreply.github.com>
2022-06-30 23:36:27 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprint(stderr,"\n");
 return (1);
 }
