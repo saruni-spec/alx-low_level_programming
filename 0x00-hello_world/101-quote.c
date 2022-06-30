53151de0bd613b81dde993980a251e7c0ec24314
saruni-spec <54449536+saruni-spec@users.noreply.github.com>
2022-06-30 23:25:11 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - print tp stderr
- * Return: Always 1 
- */
+* main - print tp stderr
+* Return: Always 1 
+*/
 int main(void)
 {
-fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+fprintf(stderr, "and that piece of art is useful" );
+fprint (stderr, "- Dora Korpar, 2015-10-19\n");
+  return (1);
 }
