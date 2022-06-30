b6ac86bacba9836048f11daa8fe96bbbd5fdb44d
saruni-spec <54449536+saruni-spec@users.noreply.github.com>
2022-06-30 15:07:39 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+/*prints using puts*/
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
