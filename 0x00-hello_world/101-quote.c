b223981be00463784f5774ab88ddec3ec5dcf2e9
saruni-spec <54449536+saruni-spec@users.noreply.github.com>
2022-06-30 23:20:38 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 /**
  * main - print tp stderr
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-fwrite(STDOUT_FILENO, "and that piece of art is useful
-\" - Dora Korpar, 2015-10-19\n", 59);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
