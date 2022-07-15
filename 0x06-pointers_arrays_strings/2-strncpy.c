67a59192c71689664106fc4a0cc5f3db9c547a17
saruni-spec <smithsaruni16@gmail.com>
2022-07-15 15:08:27 +0300

    6

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,24 @@
 #include "main.h"
-<<<<<<< HEAD
-
-=======
->>>>>>> 8a19de683009727cfb00b761fe17e73ee7ee2496
 /**
- * _strncpy - Entry point
- * @dest: char pinter
- * @src: char pointer
- * @n: integer
- *
- * Return: char
+ *_strncpy - copies a string
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	for (; i < n; i++)
-		dest[i] = '\0';
-
-	return (dest);
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
